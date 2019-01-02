bc42f962f63be2352b5b1afde6018872c5817d3a
Mannix <jun2675731483@gmail.com>
2019-01-02 11:46:29 +0800

    fix config parsing

--- client/config.go
+++ client/config.go
@@ -2,11 +2,13 @@ package client
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/url"
+	"regexp"
 	"sync"
 )
 
@@ -27,18 +29,18 @@ type Remote struct {
 
 func NewConfig(filename string) (config Config, err error) {
 	var (
-		confBytes []byte
-		allRemote = []Remote{}
-		wg        = &sync.WaitGroup{}
-		locker    = &sync.Mutex{}
+		confString string
+		allRemote  = []Remote{}
+		wg         = &sync.WaitGroup{}
+		locker     = &sync.Mutex{}
 	)
 
-	confBytes, err = ioutil.ReadFile(filename)
+	confString, err = prepareConfig(filename)
 	if err != nil {
 		return
 	}
 
-	err = json.Unmarshal(confBytes, &config)
+	err = json.Unmarshal([]byte(confString), &config)
 	if err != nil {
 		return
 	}
@@ -74,6 +76,7 @@ func prepareRemote(remote Remote) (remotes []Remote) {
 		err   error
 		u     *url.URL
 		addrs []string
+		port  string
 	)
 	defer func() {
 		if len(remotes) <= 0 {
@@ -94,21 +97,59 @@ func prepareRemote(remote Remote) (remotes []Remote) {
 		return
 	}
 
+	port = u.Port()
+	if len(port) <= 0 {
+		switch u.Scheme {
+		case "http":
+			port = "80"
+		case "https":
+			port = "443"
+		default:
+			err = errors.New(fmt.Sprint("not support protocol ", u.Scheme))
+			return
+		}
+	}
+
 	addrs, err = net.LookupHost(u.Hostname())
 	if err != nil {
 		return
 	}
 
 	for _, addr := range addrs {
-		if u.Scheme == "https" {
-			addr = fmt.Sprintf("%s:443", addr)
-		} else {
-			addr = fmt.Sprintf("%s:80", addr)
-		}
+
+		addr = fmt.Sprintf("%s:%s", addr, port)
 
 		remote.IP = addr
 		remotes = append(remotes, remote)
 	}
+
+	return
+}
+
+func prepareConfig(confPath string) (conf string, err error) {
+	var (
+		confBytes []byte
+		re        *regexp.Regexp
+	)
+
+	confBytes, err = ioutil.ReadFile(confPath)
+	if err != nil {
+		return
+	}
+
+	conf = string(confBytes)
+
+	re, err = regexp.Compile(`\s+//.*`)
+	if err != nil {
+		return
+	}
+	conf = re.ReplaceAllString(conf, "")
+
+	re, err = regexp.Compile(`,\s*\n*}`)
+	if err != nil {
+		return
+	}
+	conf = re.ReplaceAllString(conf, "\n}")
 
 	return
 }
