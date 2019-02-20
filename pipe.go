4443e6ba50c8ee40a7a78c0fb2bd39c804576965
Mannix <jun2675731483@gmail.com>
2019-02-20 14:35:37 +0800

    no message

--- pipe.go
+++ pipe.go
@@ -2,31 +2,14 @@ package ladder
 
 import (
 	"io"
-	"sync"
 )
 
-func Pipe(src io.ReadWriteCloser, dst io.ReadWriteCloser) (int64, int64) {
-
-	var sent, received int64
-	var c = make(chan bool)
-	var o sync.Once
-
-	close := func() {
-		src.Close()
-		dst.Close()
-		close(c)
-	}
-
+func Pipe(src io.ReadWriteCloser, dst io.ReadWriteCloser) {
 	go func() {
-		received, _ = io.Copy(src, dst)
-		o.Do(close)
+		buffer := make([]byte, 1024)
+		io.CopyBuffer(src, dst, buffer)
 	}()
 
-	go func() {
-		sent, _ = io.Copy(dst, src)
-		o.Do(close)
-	}()
-
-	<-c
-	return sent, received
+	buffer := make([]byte, 1024)
+	io.CopyBuffer(dst, src, buffer)
 }
