81138b145d8b334847254cf6d4f3febf8d415d9a
Mannix <jun2675731483@gmail.com>
2019-02-25 19:37:56 +0800

    no message

--- pipe.go
+++ pipe.go
@@ -6,10 +6,13 @@ import (
 
 func Pipe(src io.ReadWriteCloser, dst io.ReadWriteCloser) {
 	go func() {
-		buffer := make([]byte, 1024)
+		buffer := make([]byte, 1024*4)
 		io.CopyBuffer(src, dst, buffer)
 	}()
 
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024*4)
 	io.CopyBuffer(dst, src, buffer)
+
+	src.Close()
+	dst.Close()
 }
